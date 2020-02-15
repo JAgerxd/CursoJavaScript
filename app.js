a611a9bfe1518d606cfa2122b036dd24db602d81
Ignacio Guerrero <ignacio.guerrero.o@gmail.com>
2020-02-14 22:02:47 -0300

    retorno de funciones

--- app.js
+++ app.js
@@ -1,27 +1,60 @@
-//funciones anonima y explicita
+// retorno de funciones (primitivos,undefined, objeto o funcion, nulo )
 
-function imprimir(fn) {
-    fn();
-
-}
-
-
-var persona = {
-    nombre: "Juan",
-    apellido: "Padilla"
+function obtenerAleatorio() {
+    return Math.random();
 
 }
 
-var miFuncion = function(){
-    console.log("miFuncion");
+function obtenerNombre() {
+    return "Juan";
+}
+
+function esMayor05() {
+
+    if (obtenerAleatorio() > 0.5) {
+        return true;
+
+    } else {
+        return false;
+    }
 
 }
 
-imprimir(miFuncion);
+if (esMayor05()) {
+    console.log("Es Mayor a 0.5");
+} else {
+    console.log("Es menor a 0.5");
+}
 
 
 
-/*imprimir(function () {
+function crearPersona(nombre, apellido) {
 
-    console.log("funcion anonima");
-});*/
+    return {
+        nombre: nombre,
+        apellido: apellido
+    }
+
+}
+
+var persona = crearPersona("Maria", "Paz");
+
+//console.log(persona.nombre);
+//console.log(persona.apellido);
+
+
+function creaFunction() {
+    return function (nombre) {
+        console.log("Me creo!!!" + nombre);
+
+        return function () {
+            console.log("Segunda Funcion");
+        }
+    }
+}
+
+var nuevaFuncion = creaFunction();
+var segundaFuncion = nuevaFuncion(persona.nombre);
+
+segundaFuncion();
+
