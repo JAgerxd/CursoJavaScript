a666d47562800c93dfe21c2348e4bd0f69ca9caa
Ignacio Guerrero <ignacio.guerrero.o@gmail.com>
2020-02-14 21:31:57 -0300

    funciones2

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 //funciones
 
+function imprimir(persona /* , apellido */) {
+
+    console.log(persona.nombre + " " + persona.apellido);
+
+    persona.nombre ="Maria";
+
+    // apellido = apellido || "xxx"  //Forma 2
+
+    /*if( apellido === undefined){  Forma 1
+        apellido = "xxx";
+    }*/
 
 
-function primeraFuncion() {
-
-    var a = 20;
-    console.log(a);
+    //console.log(nombre + " " + apellido);
 
 }
 
-primeraFuncion();
+var obj = {
 
+    nombre: "Juan",
+    apellido: "Padilla"
+
+}
+
+// variable explicita 
+// var nombre = "Juan";
+
+/*
+imprimir({
+
+    nombre: "Juan",
+    apellido: "Padilla"
+
+}); //variable anonima */
+
+imprimir(obj);
+
+console.log(obj);
