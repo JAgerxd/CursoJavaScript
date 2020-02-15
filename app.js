b631a728a0017d4e0a7ca97eb63e24cfc4bdc6b7
Ignacio Guerrero <ignacio.guerrero.o@gmail.com>
2020-02-15 15:05:23 -0300

    funciones de primera clase

--- app.js
+++ app.js
@@ -1,60 +1,19 @@
-// retorno de funciones (primitivos,undefined, objeto o funcion, nulo )
+// funciones de primera clase
 
-function obtenerAleatorio() {
-    return Math.random();
+function a(){
 
-}
-
-function obtenerNombre() {
-    return "Juan";
-}
-
-function esMayor05() {
-
-    if (obtenerAleatorio() > 0.5) {
-        return true;
-
-    } else {
-        return false;
-    }
+console.log("funcion a");
 
 }
 
-if (esMayor05()) {
-    console.log("Es Mayor a 0.5");
-} else {
-    console.log("Es menor a 0.5");
-}
+a();
 
-
-
-function crearPersona(nombre, apellido) {
-
-    return {
-        nombre: nombre,
-        apellido: apellido
-    }
-
-}
-
-var persona = crearPersona("Maria", "Paz");
-
-//console.log(persona.nombre);
-//console.log(persona.apellido);
-
-
-function creaFunction() {
-    return function (nombre) {
-        console.log("Me creo!!!" + nombre);
-
-        return function () {
-            console.log("Segunda Funcion");
-        }
+a.nombre = "Maria";
+a.direccion = {
+    pais: "Costa Rica",
+    ciudad: "San Jose",
+    edificio: {
+        piso: "8vo",
+        nombre: "Edificio principal"
     }
 }
-
-var nuevaFuncion = creaFunction();
-var segundaFuncion = nuevaFuncion(persona.nombre);
-
-segundaFuncion();
-
