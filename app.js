df0d85bb5cff6162415af413cf74c958eb870634
JAgerxd <Baco2639414>
2020-02-27 15:35:58 -0300

    // Funciones typeof o instanceof

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
-// Funciones Anonimas
-/* Ejemplo 1
+// Funciones typeof o instanceof
 
-
-(function () {
-
-    var a = 10;
-    console.log(a);
-
-    function cambiarA() {
-        a = 20;
-    }
-
-    cambiarA();
-    console.log(a);
-
-}());
- */
-
-
-function ejecutarFuncion(fn) {
-    if (fn() === 1) {
-        return true;
+function identifica(param) {
+    //console.log(typeof param );
+    /*if (typeof param == "function") {
+        param();
     } else {
-        return false;
+        console.log(param);
     }
 
-};
+     */
+    console.log(param instanceof Persona);
+    console.log(typeof param);
+}
 
-console.log(ejecutarFuncion(function () {
-    console.log("Funcion Anonima ejecutada");
-    return 1;
-}));
+function Persona() {
+    this.nombre = "Fernando";
+    this.edad = 30;
+
+}
+
+var fernando = new Persona();
+
+identifica(fernando);
 
 
 
